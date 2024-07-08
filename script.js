ae259a9a4f337792978ef93e1e8ef74257c085ba
José Lucas <joselucassilva567@gmail.com>
2024-07-07 21:34:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ const elementScore1 = document.getElementById('score-1');
 const current0 = document.getElementById('current-0');
 const current1 = document.getElementById('current-1');
 
+const player0 = document.querySelector('.player-0');
+const player1 = document.querySelector('.player-1');
 const elementDice = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn-new');
 const btnHold = document.querySelector('.btn-hold');
 const btnRoll = document.querySelector('.btn-roll');
 let currentScore = 0;
-
+const scores = [0, 0];
+let activePlayer = 0;
 
 elementScore0.textContent = 0;
 elementScore1.textContent = 0;
@@ -26,8 +29,12 @@ btnRoll.addEventListener('click', function () {
 
     if( dice !== 1){
         currentScore += dice;
-        current0.textContent = currentScore;
+        document.getElementById(`current-${activePlayer}`).textContent = currentScore;
     }else {
-
+        document.getElementById(`current-${activePlayer}`).textContent = 0;
+        currentScore = 0
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0.classList.toggle('player-active');
+        player1.classList.toggle('player-active');
     }
 })
