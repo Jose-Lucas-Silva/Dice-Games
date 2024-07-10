b9bfbf17277cf81db9f99814f8ec73420f4c8e9c
José Lucas <joselucassilva567@gmail.com>
2024-07-09 22:27:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,30 +11,54 @@ const elementDice = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn-new');
 const btnHold = document.querySelector('.btn-hold');
 const btnRoll = document.querySelector('.btn-roll');
+
 let currentScore = 0;
 const scores = [0, 0];
 let activePlayer = 0;
+let playing = true;
 
 elementScore0.textContent = 0;
 elementScore1.textContent = 0;
 elementDice.classList.add('hidden');
 
+const switchPlayer = function (){ document.getElementById(`current-${activePlayer}`).textContent = 0;
+    currentScore = 0
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0.classList.toggle('player-active');
+    player1.classList.toggle('player-active');
+}
 
 btnRoll.addEventListener('click', function () {
-    const dice = Math.trunc(Math.random() * 6) + 1;
+    if(playing){
+        const dice = Math.trunc(Math.random() * 6) + 1;
 
-    elementDice.classList.remove('hidden');
+        elementDice.classList.remove('hidden');
 
-    elementDice.src = `dice-${dice}.png`;
+        elementDice.src = `dice-${dice}.png`;
 
-    if( dice !== 1){
-        currentScore += dice;
-        document.getElementById(`current-${activePlayer}`).textContent = currentScore;
-    }else {
-        document.getElementById(`current-${activePlayer}`).textContent = 0;
-        currentScore = 0
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        player0.classList.toggle('player-active');
-        player1.classList.toggle('player-active');
+        if( dice !== 1){
+            currentScore += dice;
+            document.getElementById(`current-${activePlayer}`).textContent = currentScore;
+        }else {
+            switchPlayer();
+        }
     }
+})
+
+
+btnHold.addEventListener('click', function() {
+
+if(playing){
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score-${activePlayer}`).textContent = scores[activePlayer];
+    
+    if(scores[activePlayer] >= 100) {
+        elementDice.classList.add('hidden');
+        playing = false;
+        document.querySelector(`.player-${activePlayer}`).classList.add('player-winner');
+        document.querySelector(`.player-${activePlayer}`).classList.remove('player-active');
+    }else{
+        switchPlayer();
+    }
+}
 })
